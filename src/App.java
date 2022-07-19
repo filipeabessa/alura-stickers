f1ec2d36ea6e6ea094842a9418c25d40dc05e160
filipeabessa <filipegbessa@gmail.com>
2022-07-19 15:10:58 -0300

    feat: receber e imprimir os dados dos 250 melhores filmes da api do imdb

--- src/App.java
+++ src/App.java
@@ -1,16 +1,19 @@
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
         // Fazer comunicação HTTP e buscar os top 250 filmes
         String url = "https://api.mocki.io/v2/549a5d8b";
         var client = HttpClient.newHttpClient();
         URI endereco = URI.create(url);
-        HttpRequest build = HttpRequest.newBuilder(endereco).GET().build();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
         // pegar só os dados que interessam (titulo, poster, classificação, ano)
 
         // exibir e manipular os dados
